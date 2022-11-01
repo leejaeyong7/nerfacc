0a439b4591f0658b0c509d840e38b3441aec2317
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-01 03:28:23 -0500

    updating orders

--- examples/run.sh
+++ examples/run.sh
@@ -1,4 +1,4 @@
-SCENE_IDS=(chair ficus hotdog lego materials mic ship drums)
+SCENE_IDS=(chair drums ficus hotdog lego materials mic ship)
 for SCENE in ${SCENE_IDS[@]}; do
     python train_freq_mlp_nerf.py --data_path=/mnt/data/nerf_synthetic --log_path=logs --run_name=freq_train_$SCENE --scene=$SCENE --train_split=train
 done
