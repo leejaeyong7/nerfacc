755b6d2f0bf7469c897e01e83da19bb2c3463b13
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-10 07:49:49 -0600

    chenging runs

--- examples/run_ablations_depth_2d.sh
+++ examples/run_ablations_depth_2d.sh
@@ -1,5 +1,5 @@
 SCENE_IDS=(chair drums ficus hotdog lego materials mic ship)
-DEPTHS=(2 3 4)
+DEPTHS=(3 4)
 for SCENE in ${SCENE_IDS[@]}; do
     for DEPTH in ${DEPTHS[@]}; do
         python train_freqs.py --data_path=data/nerf_synthetic --run_name=freq_2d_l7_f16_d${DEPTH}_${SCENE} --scene=${SCENE} --train_split=train --model_type=2d --net_depth=$DEPTH
