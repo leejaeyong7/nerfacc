06820b081bc54f89b839f9f78ea7dca4fd4e9aa8
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-10 16:44:44 -0600

    updating typo

--- examples/run_dense_2d.sh
+++ examples/run_dense_2d.sh
@@ -1,4 +1,4 @@
 SCENE_IDS=(lego)
 for SCENE in ${SCENE_IDS[@]}; do
-    python train_freqs_dense.py --data_path=data/nerf_synthetic --run_name=freq_2d_l7_f16_d2_$SCENE --scene=$SCENE --train_split=train --model_type=2d
+    python train_freqs_dense.py --data_path=data/nerf_synthetic --run_name=freq_2d_l7_f16_d2_$SCENE --scene=$SCENE --train_split=train --model_type=2d --net_depth=2
 done
