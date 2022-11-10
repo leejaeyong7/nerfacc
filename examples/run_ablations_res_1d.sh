335690cef3efc32c8756cf4e5504b14039e955bc
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-10 00:14:25 -0600

    updating

--- examples/run_ablations_res_1d.sh
+++ examples/run_ablations_res_1d.sh
@@ -3,7 +3,6 @@ RESS=(4 10)
 for SCENE in ${SCENE_IDS[@]}; do
     for RES in ${RESS[@]}; do
         python train_freqs.py --data_path=data/nerf_synthetic --run_name=freq_1d_l7_f${RES}_d2_$SCENE --scene=$SCENE --train_split=train --model_type=1d --log2_res=$RES
-        python train_freqs.py --data_path=data/nerf_synthetic --run_name=freq_2d_l7_f${RES}_d2_$SCENE --scene=$SCENE --train_split=train --model_type=2d --log2_res=$RES
     done
 done
 
