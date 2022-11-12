d4e763f60950153c07d2dd3f0e9388f437811911
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-11 20:05:50 -0600

    dense at 0

--- examples/run_dense.sh
+++ examples/run_dense.sh
@@ -1,6 +1,4 @@
 SCENE_IDS=(lego)
 for SCENE in ${SCENE_IDS[@]}; do
-    # python train_mlp_nerf.py --data_path=data/nerf_synthetic --run_name=van_300_$SCENE --scene=$SCENE --train_split=train
-    python train_freqs_dense.py --data_path=data/nerf_synthetic --run_name=freq_2d_300_l7_f16_d2_$SCENE --scene=$SCENE --train_split=train --model_type=2d --net_depth=2
     python train_freqs_dense.py --data_path=data/nerf_synthetic --run_name=freq_1d_300_l7_f16_d8_$SCENE --scene=$SCENE --train_split=train --model_type=1d
 done
--- examples/train_freqs_dense.py
+++ examples/train_freqs_dense.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     step = 0
     tic = time.time()
     global_it = tqdm(range(max_steps), dynamic_ncols=True)
-    val_steps = [100, 300, 500]
+    val_steps = [0, 100, 300]
 
     output_folder = Path(args.output_path)
 
