f3598708a45563915c59a5ef40ec5dd9b1b604c8
Jae Yong Lee <leejaeyong7@gmail.com>
2022-12-05 03:34:21 -0600

    adding eth3d training

--- examples/run_ablations_features_8_1d.sh
+++
@@ -1,6 +0,0 @@
-SCENE_IDS=(mic lego chair drums ficus hotdog materials ship)
-FEATURE=8
-for SCENE in ${SCENE_IDS[@]}; do
-    python train_freqs.py --data_path=data/nerf_synthetic --run_name=freq_1d_l7_f${FEATURE}_d2_$SCENE --scene=$SCENE --train_split=train --model_type=1d --num_f=$FEATURE
-done
-
--- examples/run_dense_1d.sh
+++
@@ -1,4 +0,0 @@
-SCENE_IDS=(chair drums ficus hotdog lego materials mic ship)
-for SCENE in ${SCENE_IDS[@]}; do
-    python train_freqs_dense.py --data_path=data/nerf_synthetic --run_name=freq_1d_l7_f16_d2_$SCENE --scene=$SCENE --train_split=train --model_type=1d
-done
